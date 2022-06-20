42501c0560691a2ee10fabc4e08c64360016e86d
Evan Mitchell <evanm4183@gmail.com>
2022-06-20 10:53:50 -0500

    HTML and CSS skeleton

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+import { renderAllHTML } from "./renderHTML.js";
+
+const mainElement = document.querySelector("#container")
+mainElement.innerHTML = renderAllHTML()
---
+++ scripts/renderHTML.js
@@ -0,0 +1,27 @@
+export const renderAllHTML = () => {
+    return `
+    <article class="boxes">
+        <section class="selection-box">
+            <h2 class="selection-title">Paints</h2>
+
+        </section>
+        <section class="selection-box">
+            <h2 class="selection-title">Interior</h2>
+
+        </section>
+        <section class="selection-box">
+            <h2 class="selection-title">Wheels</h2>
+
+        </section>
+        <section class="selection-box">
+            <h2 class="selection-title">Technologies</h2>
+
+        </section>
+    </article>
+    <button class="order-button">Place Car Order</button>
+    <article class="custom-car-orders">
+        <h2 class="custom-car-orders-title">Custom Car Orders</h2>
+
+    </article>
+    `
+}
